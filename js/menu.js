9e6cd651d954a6b7941e71f3ff05aa5908dd6582
Julien Chambers <pdxchambers@gmail.com>
2016-11-09 17:16:40 -0800

    Added responsiveness
    Added @media queries to handle small devices.

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,16 @@
 jQuery( document ).ready( function( $ ) {
 	$('#site-nav li.menu-item-has-children').append('<span class="down-triangle"></span>');
 	
-	if (window.innerWidth < 768 ) {
+	if (window.innerWidth < 631 ) {
 		$("#site-nav").addClass("js").before('<div id="menu">&#9776;</div>');
 		$("#menu").click(function(){
 			$("#site-nav").toggle();
 		});
 	}
-		$(window).resize(function(){
-			if(window.innerWidth > 768) {
-				$("#site-nav").removeAttr("style");
-			}
-		});	
+	
+	$(window).resize(function(){
+		if(window.innerWidth > 768) {
+			$("#site-nav").removeAttr("style");
+		}
+	});	
 } );
