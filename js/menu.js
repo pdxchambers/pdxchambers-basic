488a6768224c97120ef0fdc8bf57ddac03283ef2
Julien Chambers <pdxchambers@gmail.com>
2016-11-21 06:57:17 -0800

    Fixed Navigation
    Fixed navigation to take care of issue where hover effects aren't
    triggered on touch devices. Switched top level pop-up navigation to
    trigger on click instead of on mouse hover.

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,17 @@
 jQuery( document ).ready( function( $ ) {
+	
+	$('#site-nav > ul > li.menu-item-has-children > a').click(function (e){
+		e.preventDefault();
+		$(this).parent().children('ul.sub-menu').toggle();
+		$(this).parent().siblings().children('ul.sub-menu').hide();
+	});
+	
+	$('ul.sub-menu > li.menu-item-has-children > a').click(function (e) {
+		e.preventDefault();
+		$(this).parent().children('ul.sub-menu').toggle();
+		$(this).parent().children('ul.sub-menu').find('ul.sub-menu').hide();
+	});
+	
 	$('#site-nav li.menu-item-has-children').append('<span class="down-triangle"></span>');
 	
 	if (window.innerWidth < 631 ) {
