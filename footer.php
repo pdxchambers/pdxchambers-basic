a03ff498a8ad1a7d8412f1a8f95389e66bbea43b
Julien Chambers <pdxchambers@gmail.com>
2016-11-30 23:18:46 -0800

    Pre-Testing Commit
    Final commit before testing.

--- footer.php
+++ footer.php
@@ -7,8 +7,8 @@
  * @since PDXChambers Basic 1.0
  */
 ?>
-<footer id="site-footer">
-			<div><p>Theme proudly powered by WordPress and was developed by <a href="http://www.pdxchambers.com">Julien Chambers</a></p></div>	
+			<footer id="site-footer">
+				<div><p>Theme proudly powered by WordPress and was developed by <a href="http://www.pdxchambers.com">Julien Chambers</a></p></div>	
 			</footer>
 		</div><!-- end site-container -->
 	</body>
